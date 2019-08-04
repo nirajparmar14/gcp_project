7d86586d04bb026106c30b859cfcba5eab05eef4
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 16:45:19 +0530

    Update gs_script.sh

--- gs_script.sh
+++ gs_script.sh
@@ -4,4 +4,4 @@ yum install -y epel-release
 yum install -y python-pip
 pip install --upgrade pip
 pip install apache-beam==2.13.0
-#python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
+python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
