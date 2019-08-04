04245e6da9c507c4707015a31428012af9e3cae6
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 16:07:13 +0530

    Install python pip

--- gs_script.sh
+++ gs_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cat /etc/redhat-release
+sudo yum install python-pip
 pip install apache-beam[gcp]
 python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
