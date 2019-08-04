93400b822e645d65334f43fdea8eb4515387f6fe
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 16:13:07 +0530

    Installing required package

--- gs_script.sh
+++ gs_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat /etc/redhat-release
-yum install epel-release
-yum install python-pip
+yum install -y epel-release
+yum install -y python-pip
 pip install apache-beam[gcp]
 python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
