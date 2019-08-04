0cab71bbaa1882a2625995fec407e707a71dad40
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 18:01:45 +0530

    Update gs_script.sh

--- gs_script.sh
+++ gs_script.sh
@@ -5,17 +5,4 @@ yum install -y python-pip
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install apache-beam[gcp]
-tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
-yum clean all
-yum  repolist
-yum install -y google-cloud-sdk
 python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://wordcount_template/staging     --temp_location gs://wordcount_template/temp     --template_location gs://wordcount_template/templates/wordcount
