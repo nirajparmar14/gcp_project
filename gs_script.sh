3aba3fee59cf1494ec86ad95d5ad19b0ec34c335
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 16:03:40 +0530

    Install Apache beam

--- gs_script.sh
+++ gs_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -v
+pip install apache-beam[gcp]
 python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
