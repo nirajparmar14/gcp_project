c7bd752f62274799b5786fc1666b49a79d4c7eff
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-04 12:18:23 +0530

    Word count

--- gs_script.sh
+++ gs_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install python
 python -v
+python  wordcount.py     --runner DataflowRunner     --project adminmediaanalytics     --staging_location gs://niraj_ad/staging     --temp_location gs://niraj_ad/temp     --template_location gs://niraj_ad/templates/wordcount
