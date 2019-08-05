4c549294cb8f471a5ff94f1a22e1f9e9925f4ed8
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-05 10:07:53 +0530

    Project ID change

--- gs_script.sh
+++ gs_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo $PROJECT_ID
+project_id=$PROJECT_ID
+echo $project_id
 cat /etc/redhat-release
 yum install -y epel-release
 yum install -y python-pip
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install apache-beam[gcp]
-python  wordcount.py --runner DataflowRunner --project $PROJECT_ID --staging_location gs://wordcount_template/staging --temp_location gs://wordcount_template/temp --template_location gs://wordcount_template/templates/wordcount
+python  wordcount.py --runner DataflowRunner --project $project_id --staging_location gs://wordcount_template/staging --temp_location gs://wordcount_template/temp --template_location gs://wordcount_template/templates/wordcount
