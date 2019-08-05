cd393e3db4f0c75afe1d077fa654e49f54deca5f
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2019-08-05 10:10:29 +0530

    Update gs_script.sh

--- gs_script.sh
+++ gs_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 project_id=$PROJECT_ID
 echo $project_id
+echo $$BUILD_ID
 cat /etc/redhat-release
 yum install -y epel-release
 yum install -y python-pip
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install apache-beam[gcp]
-python  wordcount.py --runner DataflowRunner --project $project_id --staging_location gs://wordcount_template/staging --temp_location gs://wordcount_template/temp --template_location gs://wordcount_template/templates/wordcount
+#python  wordcount.py --runner DataflowRunner --project $project_id --staging_location gs://wordcount_template/staging --temp_location gs://wordcount_template/temp --template_location gs://wordcount_template/templates/wordcount
